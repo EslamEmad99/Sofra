f8a7b34172e201b5baa4d0d86aa4285f6bba0f62
eslam <eslamemad903@gmail.com>
2020-08-15 12:47:21 +0200

    first commit

---
+++ app/src/main/java/eslam/emad/sofra/MainActivity.java
@@ -0,0 +1,14 @@
+package eslam.emad.sofra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
